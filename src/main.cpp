42657941919fd46b57c7d479348b0bc87e7ce096
felix-lhz <1175906605@qq.com>
2024-11-11 22:04:10 +0800

    【fixed】xbox connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,31 @@
 #include <Arduino.h>
-#include "imu.h"
-#include "xbox.h"
-#include "im948_CMD.h"
-#include "can.h"
 #include "calc.h"
+#include "can.h"
+#include "im948_CMD.h"
+#include "imu.h"
 #include "msg.h"
-
+#include "xbox.h"
 
 // Required to replace with your xbox address
 // 需要在此替换成自己的手柄蓝牙MAC地址
-XboxController xbox = XboxController("0c:35:26:e6:90:d9");
+XboxController xbox = XboxController("ac:8e:bd:14:55:d1");
 
 IMU imu;
 
+void setup() {
+    Serial.begin(115200);
 
+    xbox.connectXboxController();
 
-void setup()
-{
-  Serial.begin(115200);
-//   imu = IMU(20,19,17);
-//   xbox.connectXboxController();
-  //不支持默认参数？？？
-//   canInit(4, 5, TWAI_SPEED_1000KBPS , 10 , 10); // txPIN, rxPIN, speed, txQueue, rxQueue
-  im948Init(19, 20);
+    //   imu = IMU(20,19,17);
+    im948Init(19, 20);
+
+    //   canInit(4, 5, TWAI_SPEED_1000KBPS , 10 , 10);
+    
 }
 
-void loop()
-{
-    im948ReadData();
-    delay(100);
+void loop() {
+    Serial.println(xbox.xboxString());
+    // im948ReadData();
+    // delay(100);
 }
--- src/xbox.cpp
+++ src/xbox.cpp
@@ -3,60 +3,57 @@
 // Ensure the constructor is defined only once
 XboxController::XboxController(String targetDeviceAddress)
 {
-    xbox_controller = XboxSeriesXControllerESP32_asukiaaa::Core(targetDeviceAddress);
     target_address = targetDeviceAddress;
+    xbox_controller = new XboxSeriesXControllerESP32_asukiaaa::Core(target_address);
 }
 
 void XboxController::connectXboxController()
 {
-    xbox_controller.begin();
+    xbox_controller->begin();
     while(!is_connected)
     {
-        xbox_controller.onLoop();
-        if (xbox_controller.isConnected())
-        {
-            if (xbox_controller.isWaitingForFirstNotification())
-            {
+        xbox_controller->onLoop();
+        if (xbox_controller->isConnected()) {
+            if (xbox_controller->isWaitingForFirstNotification()) {
                 Serial.println("waiting for first notification");
-            }
-            else
-            {
+            } else {
                 is_connected = true;
+                Serial.println("connected");
             }
-        }
-        else
-        {
+        } else {
             Serial.println("not connected");
-            if (xbox_controller.getCountFailedConnection() > 2)
-            {
-            ESP.restart();
+            if (xbox_controller->getCountFailedConnection() > 5) {
+                Serial.println("ESP restart!");
+                ESP.restart();
             }
         }
+        // delay 10ms for controller to connect
+        delay(10);
     }
 }
 
 String XboxController::xboxString(){
-    String str = String(xbox_controller.xboxNotif.btnY) + "," +
-               String(xbox_controller.xboxNotif.btnX) + "," +
-               String(xbox_controller.xboxNotif.btnB) + "," +
-               String(xbox_controller.xboxNotif.btnA) + "," +
-               String(xbox_controller.xboxNotif.btnLB) + "," +
-               String(xbox_controller.xboxNotif.btnRB) + "," +
-               String(xbox_controller.xboxNotif.btnSelect) + "," +
-               String(xbox_controller.xboxNotif.btnStart) + "," +
-               String(xbox_controller.xboxNotif.btnXbox) + "," +
-               String(xbox_controller.xboxNotif.btnShare) + "," +
-               String(xbox_controller.xboxNotif.btnLS) + "," +
-               String(xbox_controller.xboxNotif.btnRS) + "," +
-               String(xbox_controller.xboxNotif.btnDirUp) + "," +
-               String(xbox_controller.xboxNotif.btnDirRight) + "," +
-               String(xbox_controller.xboxNotif.btnDirDown) + "," +
-               String(xbox_controller.xboxNotif.btnDirLeft) + "," +
-               String(xbox_controller.xboxNotif.joyLHori) + "," +
-               String(xbox_controller.xboxNotif.joyLVert) + "," +
-               String(xbox_controller.xboxNotif.joyRHori) + "," +
-               String(xbox_controller.xboxNotif.joyRVert) + "," +
-               String(xbox_controller.xboxNotif.trigLT) + "," +
-               String(xbox_controller.xboxNotif.trigRT) + "\n";
-  return str;
+    String str = String(xbox_controller->xboxNotif.btnY) + "," +
+                 String(xbox_controller->xboxNotif.btnX) + "," +
+                 String(xbox_controller->xboxNotif.btnB) + "," +
+                 String(xbox_controller->xboxNotif.btnA) + "," +
+                 String(xbox_controller->xboxNotif.btnLB) + "," +
+                 String(xbox_controller->xboxNotif.btnRB) + "," +
+                 String(xbox_controller->xboxNotif.btnSelect) + "," +
+                 String(xbox_controller->xboxNotif.btnStart) + "," +
+                 String(xbox_controller->xboxNotif.btnXbox) + "," +
+                 String(xbox_controller->xboxNotif.btnShare) + "," +
+                 String(xbox_controller->xboxNotif.btnLS) + "," +
+                 String(xbox_controller->xboxNotif.btnRS) + "," +
+                 String(xbox_controller->xboxNotif.btnDirUp) + "," +
+                 String(xbox_controller->xboxNotif.btnDirRight) + "," +
+                 String(xbox_controller->xboxNotif.btnDirDown) + "," +
+                 String(xbox_controller->xboxNotif.btnDirLeft) + "," +
+                 String(xbox_controller->xboxNotif.joyLHori) + "," +
+                 String(xbox_controller->xboxNotif.joyLVert) + "," +
+                 String(xbox_controller->xboxNotif.joyRHori) + "," +
+                 String(xbox_controller->xboxNotif.joyRVert) + "," +
+                 String(xbox_controller->xboxNotif.trigLT) + "," +
+                 String(xbox_controller->xboxNotif.trigRT) + "\n";
+    return str;
 }
